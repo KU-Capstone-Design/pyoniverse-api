6999bd57b1ac8808262c73867634a93d824dc5c7
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-11-01 12:02:36 +0900

    [feat] business

--- tests/business/test_product.py
+++ tests/business/test_product.py
@@ -10,6 +10,7 @@ from chalicelib.business.product.dto.response import ProductResponseDto
 from chalicelib.converter.product import ProductConverter
 from chalicelib.persistant.asyncio.invoker import AsyncInvoker
 from chalicelib.persistant.asyncio.mongo.command_factory import AsyncMongoCommandFactory
+from chalicelib.service.constant_brand.service import AsyncConstantBrandService
 from chalicelib.service.product.service import AsyncProductService
 from tests.mock.mock import env
 
@@ -30,23 +31,25 @@ def factory(client):
 
 
 @pytest.fixture
-def invoker():
-    return AsyncInvoker()
+def product_service(factory):
+    return AsyncProductService(command_factory=factory, invoker=AsyncInvoker())
 
 
 @pytest.fixture
-def product_service(factory, invoker):
-    return AsyncProductService(command_factory=factory, invoker=invoker)
+def constant_brand_service(factory):
+    return AsyncConstantBrandService(command_factory=factory, invoker=AsyncInvoker())
 
 
-def test_product_business(product_service, loop):
+def test_product_business(product_service, constant_brand_service, loop):
     # given
     business = AsyncProductBusiness(
         product_service=product_service,
+        constant_brand_service=constant_brand_service,
         converter=ProductConverter(),
         loop=loop,
     )
     # when
-    response = business.get_detail(request=ProductRequestDto(id=1))
+    response: ProductResponseDto = business.get_detail(request=ProductRequestDto(id=1))
     # then
     assert isinstance(response, ProductResponseDto)
+    assert response.best_brand in {b.id for b in response.brands}
