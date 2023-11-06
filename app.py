0c79662d77b2119fe255d21e1aacc7211f214414
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-11-06 10:15:02 +0900

    [feat] DB Query 변경 - status 반영 (#33)

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from chalicelib.chalice import CustomChalice
@@ -6,16 +7,15 @@ from chalicelib.extern.middleware.error_handler import handle_errors
 from chalicelib.extern.middleware.response_handler import handle_response
 
 
-if os.getenv("LOG_LEVEL", "DEBUG") == "DEBUG":
-    debug = True
-else:
-    debug = False
-
-
 main_injector = MainInjector()
 main_injector.inject()
 
-app = CustomChalice(app_name="pyoniverse-api", debug=debug)
+if os.getenv("LOG_LEVEL", "DEBUG") == "DEBUG":
+    logging.basicConfig(level=logging.DEBUG, force=True)
+else:
+    logging.basicConfig(level=logging.INFO, force=True)
+
+app = CustomChalice(app_name="pyoniverse-api")
 
 app.register_controller(version="v1")
 app.register_middleware(handle_response, "http")
